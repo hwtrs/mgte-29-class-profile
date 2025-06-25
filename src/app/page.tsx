5945fae0aec0952163b37cdd9f8086f16b46e163
Omar Anwar <omaranwar04@outlook.com>
2025-06-25 09:19:19 -0400

    Bugs with sticker positining

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,49 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
 import "./landing.scss";
+import stickers from "../data/stickers";
 
 const Home = () => {
+  const [hovered, setHovered] = useState<string | null>(null);
+  const [clicked, setClicked] = useState<string | null>(null);
+
+  // TODO: fix stickers absolute positioning on screen resize, sticker hover not correct
+
   return (
     <div className="landing-container">
       <div className="book-container">
         <img src="/landing/book.png" />
         <img src="/landing/corner_book_purple.png"/>
         <img src="/landing/corner_paper.png"/>
+
+        <div className="stickers">
+          {Object.values(stickers).map((sticker) => {
+            let src = sticker.src;
+            if (clicked === sticker.className && sticker.activeSrc) {
+              src = sticker.activeSrc;
+            } else if (hovered === sticker.className && sticker.hoverSrc) {
+              src = sticker.hoverSrc;
+            }
+
+            return (
+              <a
+                href={sticker.href}
+                key={sticker.className}
+                onMouseEnter={() => setHovered(sticker.className)}
+                onMouseLeave={() => setHovered(null)}
+                onMouseDown={() => setClicked(sticker.className)}
+                onMouseUp={() => setClicked(null)}
+                onBlur={() => setClicked(null)}
+              >
+                <img src={src} className={sticker.className} />
+              </a>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Home;
---
+++ src/data/stickers.tsx
@@ -0,0 +1,67 @@
+const stickers = {
+  about: {
+    src: "/landing/stickers/about/default.png",
+    hoverSrc: "/landing/stickers/about/hover.png",
+    activeSrc: "/landing/stickers/about/click.png",
+    className: "about-sticker",
+    href: "/about"
+  },
+  academics: {
+    src: "/landing/stickers/academics/default.png",
+    hoverSrc: "/landing/stickers/academics/hover.png",
+    activeSrc: "/landing/stickers/academics/click.png",
+    className: "academics-sticker",
+    href: "/academics"
+  },
+  awards: {
+    src: "/landing/stickers/awards/default.png",
+    hoverSrc: "/landing/stickers/awards/hover.png",
+    activeSrc: "/landing/stickers/awards/click.png",
+    className: "awards-sticker",
+    href: "/awards"
+  },
+  co_op: {
+    src: "/landing/stickers/co-op/default.png",
+    hoverSrc: "/landing/stickers/co-op/hover.png",
+    activeSrc: "/landing/stickers/co-op/click.png",
+    className: "co-op-sticker",
+    href: "/co-op"
+  },
+  demographics: {
+    src: "/landing/stickers/demographics/default.png",
+    hoverSrc: "/landing/stickers/demographics/hover.png",
+    activeSrc: "/landing/stickers/demographics/click.png",
+    className: "demographics-sticker",
+    href: "/demographics"
+  },
+  gallery: {
+    src: "/landing/stickers/gallery/default.png",
+    hoverSrc: "/landing/stickers/gallery/hover.png",
+    activeSrc: "/landing/stickers/gallery/click.png",
+    className: "gallery-sticker",
+    href: "/gallery"
+  },
+  lifestyle: {
+    src: "/landing/stickers/lifestyle/default.png",
+    hoverSrc: "/landing/stickers/lifestyle/hover.png",
+    activeSrc: "/landing/stickers/lifestyle/click.png",
+    className: "lifestyle-sticker",
+    href: "/lifestyle"
+  },
+  manchester: {
+    src: "/landing/stickers/manchester/default.png",
+    hoverSrc: "/landing/stickers/manchester/hover.png",
+    activeSrc: "/landing/stickers/manchester/click.png",
+    className: "manchester-sticker",
+    href: "https://www.youtube.com/watch?v=Zw6UpeFCeN0"
+  },
+  pre_mgte: {
+    src: "/landing/stickers/pre-mgte/default.png",
+    hoverSrc: "/landing/stickers/pre-mgte/hover.png",
+    activeSrc: "/landing/stickers/pre-mgte/click.png",
+    className: "pre-mgte-sticker",
+    href: "/pre-mgte"
+  }
+};
+
+export default stickers;
